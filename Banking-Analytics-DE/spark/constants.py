49ef23197bed73ca4829effa85f653bc8874707d
Ashwini203205 <ashwinigiri203205@gmail.com>
2026-07-17 15:05:33 +0530

    Remove duplicate Banking-Analytics-DE folder

--- Banking-Analytics-DE/spark/constants.py
+++
@@ -1,5 +0,0 @@
-SOURCE_FILE = "data/source/bank-full.csv"
-
-BRONZE_TABLE = "bronze.customer_raw"
-
-APP_NAME = "Banking Bronze Pipeline"
--- Banking-Analytics-DE/spark/db_connection.py
+++
@@ -1,7 +0,0 @@
-from config import *
-
-connection_properties = {
-    "user": DB_USER,
-    "password": DB_PASSWORD,
-    "driver": JDBC_DRIVER
-}
